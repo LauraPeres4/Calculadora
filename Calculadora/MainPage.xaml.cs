5e7f6ecca701e74bb6fe3206ad9c715a16082cee
Usuario <Usuario@CPD03-M06>
2024-03-25 15:57:39 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace Calculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void maismenos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void porcento_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dividir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
